19cd573f5c213ba1bb2ec789fb64bfb7fad2fa28
subrahmanyam <ogirala.subbu99@yahoo.com>
2018-06-27 12:42:45 +0530

    only for testing purpose

--- src/main/java/com/demo/test/HelloWorld.java
+++ src/main/java/com/demo/test/HelloWorld.java
@@ -2,7 +2,7 @@ package com.demo.test;
 
 public class HelloWorld {
 public static void main(String args[]) {
-	System.out.println("Hi This Is Testing Purpose....");
+	System.out.println("Hi This Is Testing Purpose...***.");
 	System.out.print("Hello World");
 }
 }
