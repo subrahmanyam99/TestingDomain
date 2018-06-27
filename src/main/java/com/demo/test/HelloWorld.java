4b940cd3bb237e221c3c81526816cb260716f52a
subrahmanyam.o <subrahmanyam.o@SON16095.SONATA.LOCAL>
2018-06-27 11:19:57 +0530

    Test case

--- src/main/java/com/demo/test/HelloWorld.java
+++ src/main/java/com/demo/test/HelloWorld.java
@@ -2,6 +2,7 @@ package com.demo.test;
 
 public class HelloWorld {
 public static void main(String args[]) {
-	System.out.print("Hello World..**...");
+	System.out.println("Hi This Is Testing Purpose");
+	System.out.print("Hello World");
 }
 }
