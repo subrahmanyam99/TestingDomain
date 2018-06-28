2ba60c2fc82255db1b69e8d71728cba2949fe933
subrahmanyam <ogirala.subbu99@yahoo.com>
2018-06-28 16:45:34 +0530

    maven with pmd testing

--- src/main/java/com/demo/test/HelloWorld.java
+++ src/main/java/com/demo/test/HelloWorld.java
@@ -1,8 +1,11 @@
 package com.demo.test;
 
 public class HelloWorld {
+	String name;
 public static void main(String args[]) {
 	System.out.println("*********Hii*********");
 	System.out.print("Hello World");
+	System.out.print("Hello World");
+
 }
 }
