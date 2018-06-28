9d41975f3177d127419641f5c9f63e5966f1dd07
subrahmanyam <ogirala.subbu99@yahoo.com>
2018-06-28 16:46:24 +0530

    maven with pmd report testing

---
+++ src/main/java/com/demo/test/Test.java
@@ -0,0 +1,10 @@
+package com.demo.test;
+
+public class Test {
+	public static void main(String args[]) {
+		System.out.println("*********Hii*********");
+		System.out.print("Hello World");
+		System.out.print("Hello World");
+
+	}
+}
