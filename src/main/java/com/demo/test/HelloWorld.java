8ecb54db29d4b4a22aa0533369cb4d533eb80bc8
subrahmanyam <ogirala.subbu99>
2018-06-29 10:30:26 +0530

    Changes in HelloWorld.java

--- src/main/java/com/demo/test/HelloWorld.java
+++ src/main/java/com/demo/test/HelloWorld.java
@@ -3,7 +3,7 @@ package com.demo.test;
 public class HelloWorld {
 	static String name;
 public static void main(String args[]) {
-	System.out.println("*********(Hii)*********"+name);
+	System.out.println("*********(Hii-this is for testing purpose)*********"+name);
 	System.out.print("Hello World");
 	System.out.print("Hello World");
 
